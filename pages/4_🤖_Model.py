9e17b95c95e70109557a9b5bc7ceb7f6ede90f98
NPriyankaDS <nprinka235@gmail.com>
2024-06-11 22:12:11 +0530

    minor changes to the model pages

--- pages/4_🤖_Model.py
+++ pages/4_🤖_Model.py
@@ -52,6 +52,8 @@ def main():
     ### Enter the required features to predict the total imputed COVID-19 cases.
     Please provide the values for the following features:
     """)
+    st.write("**Note:** The minimum values for 'fullyVaccinated', 'partiallyVaccinated' , 'totalVaccinations' and 'totalTests' are the last known values as on 21st April 2024.")
+
     st.divider()
     
     col1,col2 = st.columns(2)
@@ -63,25 +65,22 @@ def main():
     ]
     
     with col1:
-        fullyVaccinated = st.number_input("**fullyVaccinated**", min_value=0.0, help="Number of individuals who have completed the full vaccination regimen for COVID-19")
+        fullyVaccinated = st.number_input("**fullyVaccinated**", min_value=9327654, step=1, help="Number of individuals who have completed the full vaccination regimen for COVID-19")
         new_deaths_smoothed = st.number_input("**new_deaths_smoothed**", min_value=0.0, help="New deaths attributed to COVID-19 (7-day smoothed). Counts can include probable deaths, where reported.")
         new_people_vaccinated_smoothed = st.number_input("**new_people_vaccinated_smoothed**", min_value=0.0, help="Daily number of people receiving their first vaccine dose(7-day smoothed)")
         new_vaccinations_smoothed = st.number_input("**new_vaccinations_smoothed**", min_value=0.0, help="New COVID-19 vaccination doses administered (7-day smoothed)")
-        partiallyVaccinated = st.number_input("**partiallyVaccinated**", min_value=0.0, help="Number of individuals who have received at least one dose of a COVID-19 vaccine but have not yet completed the full vaccination regimen.")
+        partiallyVaccinated = st.number_input("**partiallyVaccinated**", min_value=4663827, step=1, help="Number of individuals who have received at least one dose of a COVID-19 vaccine but have not yet completed the full vaccination regimen.")
         stringency_index = st.number_input("**stringency_index**", min_value=0.0, max_value=100.0, help="Government response composite measure based on 9 response indicators including school/workplace closures,and travel bans, value from 0 to 100(100=strictest)")
         test24hours = st.number_input("**test24hours**", min_value=0.0, help="Number of tests conducted in the last 24 hours")
     with col2:  
-        totalTests = st.number_input("**totalTests**", min_value=0.0, help="Total number of tests for COVID-19")
-        totalVaccinations = st.number_input("**totalVaccinations**", min_value=0.0, help="Total number of COVID-19 vaccination doses administered")
+        totalTests = st.number_input("**totalTests**", min_value=4166833, help="Total number of tests for COVID-19")
+        totalVaccinations = st.number_input("**totalVaccinations**", min_value=9982068, step=1, help="Total number of COVID-19 vaccination doses administered")
         vaccinated24hours = st.number_input("**vaccinated24hours**", min_value=0.0, help="Number of people vaccinated within a 24-hour period")
-        rfh = st.number_input("**rfh**", min_value=0.0, help="10 day rainfall in mm")
-        r3h = st.number_input("**r3h**", min_value=0.0, help="Rainfall 1-month rolling aggregation long term average in mm")
-        month = st.number_input("**month**", min_value=1, max_value=12, help="Month in the year")
-        if month in [1,3,5,7,8,10,12]:
-            day_of_week = st.number_input("**day_of_week**", min_value=1, max_value=31, help="Day in the month")
-        else:
-            day_of_week = st.number_input("**day_of_week**", min_value=1, max_value=30, help="Day in the month")
-    
+        rfh = st.number_input("**rfh**", min_value=0.0, step=0.001, help="10 day rainfall in mm")
+        r3h = st.number_input("**r3h**", min_value=0.0, step=0.001, help="Rainfall 1-month rolling aggregation long term average in mm")
+        month = st.number_input("**month**", min_value=1, max_value=12, help="The month in the year with January=1, December=12")
+        day_of_week = st.number_input("**day_of_week**", min_value=0, max_value=6, help="The day of the week with Monday=0, Sunday=6")
+        
     st.divider()
 
     input_df = pd.DataFrame({
@@ -106,7 +105,7 @@ def main():
     # Preprocess the input data
     differenced_features = {
         'fullyVaccinated': 9327654, 'new_people_vaccinated_smoothed': 959,
-        'partiallyVaccinated': 4663827, 'stringency_index': 19.89, 'totalTests': 4166833, 'totalVaccinations': 9982068
+        'partiallyVaccinated': 4663827, 'stringency_index': 13.89, 'totalTests': 4166833, 'totalVaccinations': 9982068
     }
 
     differencing_data = pd.DataFrame([differenced_features])
@@ -121,7 +120,7 @@ def main():
         st.write(input_df)
         try:
             prediction = model.predict(preprocessed_data)
-            st.success(f"Predicted Total Imputed Cases: {prediction[0]}")
+            st.success(f"Predicted Total Imputed Cases: {prediction[0]: .3f}")  #Display the results upto three decimal places
         except Exception as e:
             st.error(f"An error occurred: {e}")
 
--- pages/6_🤖_Model_Total_Deaths.py
+++ pages/6_🤖_Model_Total_Deaths.py
@@ -44,13 +44,30 @@ def preprocess_log(user_input, last_value):
     pd.Series: A new series with one preprocessed value.
     '''
 
-    # Take logarithm
-    user_input, last_value = np.log(user_input), np.log(last_value)
+    # Use np.log1p for safer logarithm calculation
+    log_user_input = np.log1p(user_input)
+    log_last_value = np.log1p(last_value)
 
     # Apply differencing
-    transformed_value = user_input - last_value
+    transformed_value = log_user_input - log_last_value
 
     return transformed_value
+
+@st.cache_resource
+def load_model():
+# Load the model
+    model_path = 'model/xgb_model_total_deaths.pkl'
+
+    if not os.path.exists(model_path):
+        st.error(f" Model file not found at {model_path}. Please check the path and try again.")
+        return
+    
+    try:
+        model = joblib.load(model_path)
+    except Exception as e:
+        st.error(f"An error occured while loading the model: {e}")
+        return
+    return model
     
 # Main function to display the Streamlit app
 def main():
@@ -60,10 +77,13 @@ def main():
     ### Enter the required features to predict the total deaths from COVID-19.
     Please provide the values for the following features:
     """)
+    st.write("**Note:** The minimum values for 'fullyVaccinated', 'partiallyVaccinated' and 'totalVaccinations' are the last known values as on 21st April 2024.")
+
+    st.divider()
 
     # Input fields for each feature
-    input_data = {}
-    columns = st.columns(2)
+    
+    col1, col2 = st.columns(2)
     feature_list = [
         'imputed_active_cases', 'fullyVaccinated', 'new_vaccinations_smoothed', 
         'partiallyVaccinated', 'stringency_index', 'test24hours', 'totalVaccinations', 
@@ -71,29 +91,50 @@ def main():
         'day_of_week', 'month'
     ]
     
-    for i, feature in enumerate(feature_list):
-        with columns[i % 2]:
-            if feature == "fullyVaccinated":
-                input_data[feature] = st.number_input(f"Enter {feature}", min_value=9327654, step=1)
-            elif feature == "partiallyVaccinated":
-                input_data[feature] = st.number_input(f"Enter {feature}", min_value=4663827, step=1)
-            elif feature == "totalVaccinations":
-                input_data[feature] = st.number_input(f"Enter {feature}", min_value=9982068, step=1)
-            elif feature in ["stringency_index", "positive_rate", "rfh", "r3h"]:
-                input_data[feature] = st.number_input(f"Enter {feature}", min_value=0.0, step=0.001)
-            elif feature == "day_of_week":
-                input_data[feature] = st.number_input(f"Enter day of week (0 to 6)", min_value=0, max_value=6, step=1)
-            elif feature == "month":
-                input_data[feature] = st.number_input(f"Enter the month (1 to 12)", min_value=1, max_value=12, step=1)
-            else:
-                input_data[feature] = st.number_input(f"Enter {feature}", min_value=0, step=1)
+    with col1:
+        
+        imputed_active_cases = st.number_input("**imputed_active_cases**", min_value=0.0, help="Estimate of the number of active COVID-19 cases at a given time")
+        fullyVaccinated = st.number_input("**fullyVaccinated**", min_value=9327654, step=1, help="Number of individuals who have completed the full vaccination regimen for COVID-19")
+        new_vaccinations_smoothed = st.number_input("**new_vaccinations_smoothed**", min_value=0.0, help="New COVID-19 vaccination doses administered (7-day smoothed)")
+        partiallyVaccinated = st.number_input("**partiallyVaccinated**", min_value=4663827, step=1, help="Number of individuals who have received at least one dose of a COVID-19 vaccine but have not yet completed the full vaccination regimen.")
+        stringency_index = st.number_input("**stringency_index**", min_value=0.0, step=0.001, max_value=100.0, help="Government response composite measure based on 9 response indicators including school/workplace closures,and travel bans, value from 0 to 100(100=strictest)")
+        test24hours = st.number_input("**test24hours**", min_value=0, help="Number of tests conducted in the last 24 hours")
+        totalVaccinations = st.number_input("**totalVaccinations**", min_value=9982068, step=1, help="Total number of COVID-19 vaccination doses administered")
 
-    # Convert input data to DataFrame
-    input_df = pd.DataFrame([input_data])
+    with col2:  
+        
+        total_tests_per_thousand = st.number_input("**total_tests_per_thousand**", min_value=0.0, help="Total tests for COVID-19 per thousand people")
+        vaccinated24hours = st.number_input("**vaccinated24hours**", min_value=0, help="Number of people vaccinated within a 24-hour period")
+        positive_rate = st.number_input("**positive_rate**", min_value=0.0, step=0.001, help="Share of COVID-19 tests that are positive in a rolling 7-day average")
+        rfh = st.number_input("**rfh**", min_value=0.0, step=0.001, help="10 day rainfall in mm")
+        r3h = st.number_input("**r3h**", min_value=0.0, step=0.001, help="Rainfall 1-month rolling aggregation long term average in mm")
+        month = st.number_input("**month**", min_value=1, max_value=12, help="The month in the year with January=1, December=12")
+        day_of_week = st.number_input("**day_of_week**", min_value=0, max_value=6, help="The day of the week with Monday=0, Sunday=6")
+        
+    st.divider()
+
+    input_df = pd.DataFrame({
+            'imputed_active_cases': [imputed_active_cases],
+            'fullyVaccinated': [fullyVaccinated],
+            'new_vaccinations_smoothed': [new_vaccinations_smoothed],
+            'partiallyVaccinated': [partiallyVaccinated],
+            'stringency_index' : [stringency_index],
+            'test24hours': [test24hours],
+            'totalVaccinations': [totalVaccinations],
+            'total_tests_per_thousand': [total_tests_per_thousand],
+            'vaccinated24hours': [vaccinated24hours],
+            'positive_rate': [positive_rate],
+            'rfh': [rfh],
+            'r3h': [r3h],
+            'day_of_week': [day_of_week],
+            'month':[month],
+            })
+    
+    st.markdown("**Note:** The non-stationary features are differenced to make the data stationary.")
 
     # Preprocess the input data
     differenced_features = {
-        'fullyVaccinated': 9327654, 'partiallyVaccinated': 4663827, 'stringency_index': 19.89, 
+        'fullyVaccinated': 9327654, 'partiallyVaccinated': 4663827, 'stringency_index': 13.89, 
         'totalVaccinations': 9982068
     }
 
@@ -108,24 +149,14 @@ def main():
     for feature in log_feature.keys():
         preprocessed_data[feature] = preprocess_log(preprocessed_data[feature], log_series[feature])
 
-    # Load the model
-    model_path = 'model/xgb_model_total_deaths.pkl'
-
-    if not os.path.exists(model_path):
-        st.error(f" Model file not found at {model_path}. Please check the path and try again.")
-        return
-    
-    try:
-        model = joblib.load(model_path)
-    except Exception as e:
-        st.error(f"An error occured while loading the model: {e}")
-        return
-
     # Make prediction
     if st.button("Predict"):
         try:
+            model = load_model()
+            st.write("**You have submitted the below data.**")
+            st.write(input_df)
             prediction = model.predict(preprocessed_data)
-            st.success(f"Predicted Total Deaths: {prediction[0]}")
+            st.success(f"Predicted Total Deaths: {prediction[0]: .3f}") #Display the results upto three decimal places
         except Exception as e:
             st.error(f"An error occurred: {e}")
 
