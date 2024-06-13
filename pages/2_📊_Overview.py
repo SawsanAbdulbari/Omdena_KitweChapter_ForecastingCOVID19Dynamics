b10e86de634ea1bdc0035b7bd1d04b9aa428c488
NPriyankaDS <nprinka235@gmail.com>
2024-06-13 19:22:04 +0530

    Implement proposed changes to overview page

--- pages/2_📊_Overview.py
+++ pages/2_📊_Overview.py
@@ -17,17 +17,51 @@ with open('./frontend/streamlit.css') as f:
 
 st.title(":bar_chart: Overview Of COVID-19 Data")
 
-st.markdown('<style>div.block-container{padding-top:3rem;color:white}</style>', unsafe_allow_html=True)
-
 # Assuming your dataset is named 'covid_data.csv'
 df = pd.read_csv("./data/preprocessed_data_updated.csv")
+#Renaming the column name "Unnamed: 0" to "date"
+df.rename(columns={"Unnamed: 0":"date"},inplace=True)
+#Casting the date column into datetime type
+df["date"] = pd.to_datetime(df["date"])
+
+# Sidebar for additional information
+st.sidebar.title('COVID-19 Dashboard')
+st.sidebar.image("./media/omdena_zambia_highres.png", use_column_width='always') 
+st.sidebar.write("This dashboard provides an overview of COVID-19 data, including cases, deaths, vaccinations, and testing trends.")
+st.sidebar.divider()
+
+# Filter options
+st.sidebar.header("Filter Options")
+
+# Date range filter
+date_range = st.sidebar.date_input(
+    "Select Date Range",
+    value=[df["date"].min(), df["date"].max()], 
+    min_value = df["date"].min(), 
+    max_value = df["date"].max(),
+    format = "MM/DD/YYYY",
+)
+
+# Ensure date_range is a tuple of start and end date
+if len(date_range) == 2:
+    start_date, end_date = date_range
+    # Convert start_date and end_date to datetime64 if they are not already
+    start_date = pd.to_datetime(start_date)
+    end_date = pd.to_datetime(end_date)
+    # Filter the DataFrame based on the selected date range
+    filtered_df = df[(df["date"] >= start_date) & (df["date"] <= end_date)]
+else:
+    # If date_range is not a tuple of start and end date, use the entire DataFrame
+    filtered_df = df
+
+st.markdown('<style>div.block-container{padding-top:3rem;color:white}</style>', unsafe_allow_html=True)
 
 # Calculate summary metrics
-total_cases = df['imputed_total_cases'].max()
-total_deaths = df['imputed_total_deaths'].max()
-total_vaccinations = df['totalVaccinations'].max()
-total_tests = df['totalTests'].max()
-total_recoveries = df['imputed_total_recoveries'].max()
+total_cases = filtered_df['imputed_total_cases'].max()
+total_deaths = filtered_df['imputed_total_deaths'].max()
+total_vaccinations = filtered_df['totalVaccinations'].max()
+total_tests = filtered_df['totalTests'].max()
+total_recoveries = filtered_df['imputed_total_recoveries'].max()
 
 # Format the metrics for better readability
 formatted_total_cases = f"{total_cases / 1e3:.1f} K"
@@ -35,14 +69,6 @@ formatted_total_deaths = f"{total_deaths / 1e3:.1f} K"
 formatted_total_vaccinations = f"{total_vaccinations / 1e6:.1f} M"
 formatted_total_tests = f"{total_tests / 1e6:.1f} M"
 formatted_total_recoveries = f"{total_recoveries / 1e3:.1f} K"
-
-
-# Display the metrics in columns
-# a1, a2, a3, a4 = st.columns(4)
-# a1.metric("Total Cases", formatted_total_cases)
-# a2.metric("Total Deaths", formatted_total_deaths)
-# a3.metric("Total Vaccinations", formatted_total_vaccinations)
-# a4.metric("Total Tests", formatted_total_tests)
 
 titles = ["Total Cases", "Total Deaths", "Total Vaccinations", "Total Tests", "Total Recoveries"]
 formatted_values = [formatted_total_cases, formatted_total_deaths,formatted_total_vaccinations,formatted_total_tests, formatted_total_recoveries]
@@ -67,57 +93,33 @@ display_kpi_metrics(formatted_values[:2], titles[:2], bg_colors[:2])
 # Second row with 3 columns
 display_kpi_metrics(formatted_values[2:6], titles[2:6], bg_colors[2:6])
 
-# Sidebar for additional information
-st.sidebar.title('COVID-19 Dashboard')
-st.sidebar.image("./media/omdena_zambia_highres.png", use_column_width='always') 
-st.sidebar.write("This dashboard provides an overview of COVID-19 data, including cases, deaths, vaccinations, and testing trends.")
-st.sidebar.divider()
-
-
-# Filter options
-st.sidebar.header("Filter Options")
-
-# Date range filter
-date_range = st.sidebar.date_input(
-    "Select Date Range", 
-    [pd.to_datetime(df["Unnamed: 0"]).min(), pd.to_datetime(df["Unnamed: 0"]).max()]
-)
-
-# Convert date_range to datetime
-start_date = pd.to_datetime(date_range[0])
-end_date = pd.to_datetime(date_range[1])
-
-df["Unnamed: 0"] = pd.to_datetime(df["Unnamed: 0"])
-df = df[(df["Unnamed: 0"] >= start_date) & (df["Unnamed: 0"] <= end_date)]
-
-# Display the first few rows of the filtered dataset to ensure the filter is applied correctly
-st.write(df.head())
-
+st.info(f"*The metrics displayed above is for the period {date_range}*")
+st.divider()
 # Visualization: Cases Over Time
 st.subheader("COVID-19 Cases Over Time")
-fig1 = px.line(df, x='Unnamed: 0', y='imputed_total_cases', labels={"Unnamed: 0": "Date", "imputed_total_cases": "Total Cases"})
+fig1 = px.line(filtered_df, x='date', y='imputed_total_cases', labels={"date": "Date", "imputed_total_cases": "Total Cases"})
 st.plotly_chart(fig1, use_container_width=True)
 
 # Visualization: Deaths Over Time
 st.subheader("COVID-19 Deaths Over Time")
-fig2 = px.line(df, x='Unnamed: 0', y='imputed_total_deaths', labels={"Unnamed: 0": "Date", "imputed_total_deaths": "Total Deaths"})
+fig2 = px.line(filtered_df, x='date', y='imputed_total_deaths', labels={"date": "Date", "imputed_total_deaths": "Total Deaths"})
 st.plotly_chart(fig2, use_container_width=True)
 
 
 
 # Visualization: Vaccinations Over Time
 st.subheader("COVID-19 Vaccinations Over Time")
-fig3 = px.line(df, x='Unnamed: 0', y='totalVaccinations', labels={"Unnamed: 0": "Date", "totalVaccinations": "Total Vaccinations"})
+fig3 = px.line(filtered_df, x='date', y='totalVaccinations', labels={"date": "Date", "totalVaccinations": "Total Vaccinations"})
 st.plotly_chart(fig3, use_container_width=True)
 
 
 # View and download data
 st.subheader("View and Download Data")
-with st.expander("View Data"):
-    st.write(df)
+with st.expander("**View Data**"):
+    st.write(filtered_df.reset_index(drop=True))
 
-    csv = df.to_csv(index=False).encode('utf-8')
-    st.download_button("Download Data", data=csv, file_name="covid_data.csv", mime="text/csv")
+    csv = filtered_df.to_csv(index=False).encode('utf-8')
+    st.download_button("**Download Data**", data=csv, file_name="covid_data.csv", mime="text/csv")
 
 
 
