3565ceb50fbd25735b391528e68e257e87f8e22e
Sawsan A. <142879097+SawsanAbdulbari@users.noreply.github.com>
2025-09-10 23:42:36 +0300

    Update 3_📈_EDA.py

--- pages/3_📈_EDA.py
+++ pages/3_📈_EDA.py
@@ -16,7 +16,7 @@ df = pd.read_csv(file_path)
 
 # Sidebar for additional information
 st.sidebar.title('COVID-19 Dashboard')
-st.sidebar.image("./media/omdena_zambia_highres.png", use_column_width='always') 
+st.sidebar.image("./media/omdena_zambia_highres.png", use_container_width='always') 
 st.sidebar.write("This dashboard provides an overview of COVID-19 data, including cases, deaths, vaccinations, and testing trends.")
 st.sidebar.divider()
 
